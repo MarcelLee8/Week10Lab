b741a16c931ac9efa903de684366c6a7d8cdf653
marce <marce@Marcel_Lee>
2022-11-13 17:13:23 -0700

    Implementation to AdminFilter and AccountService.

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -7,7 +7,7 @@ public class AccountService {
     
     public User login(String email, String password) {
         UserDB userDB = new UserDB();
-        
+
         try {
             User user = userDB.get(email);
             if (password.equals(user.getPassword())) {
@@ -15,7 +15,21 @@ public class AccountService {
             }
         } catch (Exception e) {
         }
-        
+
         return null;
+    }
+
+    public boolean isAdmin(String email) {
+        UserDB userDB = new UserDB();
+
+        try {
+            User user = userDB.get(email);
+
+            if (user.getRole().getRoleId() == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+        }
+        return false;
     }
 }
